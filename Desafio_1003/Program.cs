122dcb285df001fd881e4b774662393fe690a04b
Guilherme <guilhermebranco800@gmail.com>
2021-06-06 17:02:41 -0300

    Desafio 1003

---
+++ Desafio_1003/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Desafio_1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("SOMA = {0}", A+B);
+
+        }
+    }
+}
