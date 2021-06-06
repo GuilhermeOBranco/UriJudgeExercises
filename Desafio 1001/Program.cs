c26052137cdd21713f0dce7d6f6b6bb2fc25fb97
Guilherme <guilhermebranco800@gmail.com>
2021-06-06 16:58:06 -0300

    Exercise 1002

---
+++ Desafio 1001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace DesafiosUriJudge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var cultura = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = cultura;
+            Thread.CurrentThread.CurrentUICulture = cultura;
+
+            double n = 3.14159;
+            double area = double.Parse(Console.ReadLine());
+            double areaCalculada = (Math.Pow(area, 2)) * n;
+
+            System.Console.WriteLine("A={0:F4}", areaCalculada);
+        }
+    }
+}
