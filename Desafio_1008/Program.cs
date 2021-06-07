468654efdcccbb13a5c79cc570ecb673c30db38f
Guilherme <guilhermebranco800@gmail.com>
2021-06-06 23:02:48 -0300

    Desafio 1008

---
+++ Desafio_1008/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Desafio_1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroFuncionario = int.Parse(Console.ReadLine());
+            int horasTrabalhadas = int.Parse(Console.ReadLine());
+            double valorPorHora = double.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("NUMBER = {0} \nSALARY = U$ {1:F2}", numeroFuncionario, (horasTrabalhadas * valorPorHora));
+        }
+    }
+}
